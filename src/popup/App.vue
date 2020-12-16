18bf6fefd6f63a280db34583de4c1eb6185fb542
fuguoqiang1 <fuguoqiang1@100tal.com>
2020-12-16 23:06:14 +0800

    样式调节

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,19 +1,32 @@
 <template>
   <div class="page-popup">
-    <Card>
-      <div slot="title" class="titleContiner">
-        <img src="../assets/切换.png" alt="" class="image" />
-        <div class="title">环境切换</div>
-        <div class="switch">
-          <i-switch size="large" on-change="switchChange" v-model="switchState">
-            <span slot="open">开启</span>
-            <span slot="close">关闭</span>
-          </i-switch>
-        </div>
+    <div class="titleContiner">
+      <img src="../assets/切换.png" alt="" class="image" />
+      <div class="title">环境切换</div>
+      <div class="switch">
+        <i-switch size="large" @on-change="switchChange" v-model="switchState">
+          <span slot="open">开启</span>
+          <span slot="close">关闭</span>
+        </i-switch>
       </div>
+    </div>
+    <Card class="card">
       <div style="text-align:center">
-        <img src="../assets/logo.png" />
-        <h3>A high quality UI Toolkit based on Vue.js</h3>
+        <div class="tabContainer">
+          <Tabs value="name1" v-model="tabState" @on-click="tabClick">
+            <TabPane label="切换" name="switch"></TabPane>
+            <TabPane label="白名单" name="blackList"></TabPane>
+          </Tabs>
+        </div>
+        <div class="container">
+          <div class="list" v-show="tabState == 'switch'">
+            switch
+          </div>
+          <div class="list" v-show="tabState == 'blackList'">
+            <img src="../assets/logo.png" />
+            <h3>A high quality UI Toolkit based on Vue.js</h3>
+          </div>
+        </div>
       </div>
     </Card>
   </div>
@@ -39,7 +52,8 @@ export default {
 
   data() {
     return {
-      switchState: false
+      switchState: false,
+      tabState: "switch"
     };
   },
 
@@ -56,6 +70,9 @@ export default {
   methods: {
     switchChange() {
       console.log(this.switchState);
+    },
+    tabClick() {
+      console.log(this.tabState);
     }
   }
 };
@@ -66,9 +83,12 @@ export default {
   width: 400px;
   height: 500px;
   background: #eee;
-  padding: 6px 6px 6px 6px;
+  padding: 0px 8px 0px 8px;
   .titleContiner {
-    height: 32px;
+    height: 52px;
+    padding: 8px 0px 0px 0px;
+    width: 100%;
+    float: left;
     .image {
       width: 32px;
       height: 32px;
@@ -84,6 +104,20 @@ export default {
       height: 32px;
       float: right;
       line-height: 32px;
+    }
+  }
+  .card {
+    height: calc(100% - 60px);
+    width: 100%;
+    overflow: auto;
+    float: left;
+    .tabContainer {
+      height: 52px;
+      width: 100%;
+    }
+    .container {
+      height: calc(100% - 52px);
+      width: 100%;
     }
   }
 }
