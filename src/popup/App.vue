5ebc1f977316268f80965c702dea5e7aa4aa4ff2
fuguoqiang1 <fuguoqiang1@100tal.com>
2020-12-18 00:35:22 +0800

    添加网关

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -13,12 +13,13 @@
       </div>
     </div>
     <a-card :bordered="false" class="card">
-      <div style="text-align:center">
+      <div>
         <div class="tabContainer">
           <a-tabs @change="tabClick" v-model="tabState">
             <a-tab-pane key="switch" tab="切换"> </a-tab-pane>
             <a-tab-pane key="blackList" tab="白名单"> </a-tab-pane>
             <a-tab-pane key="gateway" tab="网关"> </a-tab-pane>
+            <a-tab-pane key="host" tab="host"> </a-tab-pane>
           </a-tabs>
         </div>
         <div class="container">
@@ -42,8 +43,58 @@
             <h3>A high quality UI Toolkit based on Vue.js</h3>
           </div>
           <div class="list" v-show="tabState == 'gateway'">
-            网关
+            <div class="addGateway">
+              <a-form-model
+                ref="ruleForm"
+                layout="inline"
+                :model="ruleForm"
+                :rules="rules"
+              >
+                <a-form-model-item prop="name">
+                  <a-input
+                    v-model="ruleForm.name"
+                    autocomplete="off"
+                    placeholder="轻输入网关名称"
+                  />
+                </a-form-model-item>
+                <a-form-model-item prop="address">
+                  <a-input
+                    v-model="ruleForm.address"
+                    autocomplete="off"
+                    placeholder="轻输入网关ip"
+                  />
+                </a-form-model-item>
+                <a-form-model-item class="buttonStyle">
+                  <a-button type="primary" @click="submitForm('ruleForm')">
+                    添加
+                  </a-button>
+                </a-form-model-item>
+              </a-form-model>
+            </div>
+            <a-table
+              :columns="columns"
+              :data-source="gatewayList"
+              :pagination="false"
+              bordered
+              rowKey="id"
+            >
+              <div slot="operation" slot-scope="status, record">
+                <template>
+                  <div class="handle" style="float:left;padding-right:12px;">
+                    <a-button type="primary" size="small">
+                      编辑
+                    </a-button>
+                  </div>
+                  <div class="handle">
+                    <a-button type="danger" size="small">
+                      删除
+                    </a-button>
+                  </div>
+                </template>
+              </div>
+            </a-table>
           </div>
+          <div class="list" v-show="tabState == 'host'"></div>
         </div>
       </div>
     </a-card>
@@ -69,10 +120,80 @@ export default {
   components: {},
 
   data() {
+    let validatePass = (rule, value, callback) => {
+      let regexp = /^((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})(\.((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})){3}$/;
+      let valdata = value.split(",");
+      let isCorrect = true;
+      if (valdata.length) {
+        for (let i = 0; i < valdata.length; i++) {
+          if (regexp.test(valdata[i]) == false) {
+            isCorrect = false;
+          }
+        }
+      }
+
+      if (value === "") {
+        callback(new Error("请输入ip地址"));
+      } else if (!isCorrect) {
+        callback(new Error("请输入正确的ip地址"));
+      } else {
+        callback();
+      }
+    };
     return {
+      columns: [
+        {
+          title: "网关名称",
+          dataIndex: "name",
+          key: "name",
+          ellipsis: true,
+          align: "center"
+        },
+        {
+          title: "网关地址",
+          dataIndex: "address",
+          key: "address",
+          ellipsis: true,
+          align: "center"
+        },
+        {
+          title: "操作",
+          dataIndex: "operation",
+          ellipsis: true,
+          width: 140,
+          scopedSlots: { customRender: "operation" },
+          fixed: "right",
+          align: "center"
+        }
+      ],
+      ruleForm: {
+        name: "",
+        address: ""
+      },
+      rules: {
+        name: [
+          {
+            required: true,
+            message: "请输入网关名称",
+            trigger: "blur"
+          }
+        ],
+        address: [{ required: true, validator: validatePass, trigger: "blur" }]
+      },
+      layout: {
+        labelCol: { span: 1 },
+        wrapperCol: { span: 10 }
+      },
       switchkey: "online",
       switchState: false,
       tabState: "switch",
+      gatewayList: [
+        {
+          name: "localhost",
+          address: "127.0.0.1",
+          id: 0
+        }
+      ],
       list: [
         {
           label: "正式环境",
@@ -101,6 +222,21 @@ export default {
   destroyed() {},
 
   methods: {
+    submitForm(formName) {
+      console.log(formName, "formName");
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          let obj = {
+            ...this.ruleForm,
+            id: this.gatewayList.length
+          };
+          this.gatewayList.push(obj);
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
     envSwitchChange(item) {
       this.switchkey = item.value;
     },
@@ -113,10 +249,10 @@ export default {
   }
 };
 </script>
-
+<style lang="less"></style>
 <style lang="less" rel="stylesheet/less" scoped>
 .page-popup {
-  width: 380px;
+  width: 520px;
   //   height: 500px;
   background: #e8eaec;
   padding: 0px 8px 8px 8px;
@@ -158,6 +294,17 @@ export default {
       .title {
         text-align: left;
       }
+    }
+  }
+  .addGateway {
+    padding: 0px 0px 12px 0px;
+    .buttonStyle {
+      float: right;
+      margin-right: 0px;
+    }
+    .handle {
+      padding-right: 12px;
+      //   float: left;
     }
   }
 }
--- src/setting/main.js
+++ src/setting/main.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
-import "./plugins/view-design";
+import "./plugins/ant-design-vue";
 Vue.config.productionTip = false;
 
 new Vue({
